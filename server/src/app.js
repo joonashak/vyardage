e53340c3effa501b1d9dcc012ca2293f601baa1b
Joonas Häkkinen <joonas@joonashakkinen.fi>
2020-03-21 21:17:29 +0200

    Basic express configuration

--- server/src/app.js
+++ server/src/app.js
@@ -1,22 +1,37 @@
 import express from 'express';
 import Knex from 'knex';
 import path from 'path';
+import cors from 'cors';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import promiseRouter from 'express-promise-router';
 import config from './utils/config';
 
 
 // Connect to database.
-export const knex = Knex({
+const knex = Knex({
   client: 'pg',
   connection: config.databaseUrl,
   debug: process.env.NODE_ENV === 'development',
 });
 
-// FIXME: remove this later on
-// Test connection
-knex.raw('select 1').then(() => console.log('db working')).catch(() => throw new Error('failed db connection'));
-
 const app = express();
 
+// CORS for development.
+if (process.env.NODE_ENV === 'development') {
+  app.use(cors({ credentials: true, origin: config.frontendUrl }));
+}
+
+// Middleware configuration.
+const logFormat = process.env.NODE_ENV === 'production' ? 'combined' : 'dev';
+const router = promiseRouter();
+
+app
+  .use(bodyParser.json())
+  .use(morgan(logFormat))
+  .use(router);
+
+// Serve static frontend.
 app.use(express.static('public'));
 app.get('*', (req, res) => res.sendFile(path.resolve('public', 'index.html')));
 
