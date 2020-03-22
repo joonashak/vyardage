0336d693ef67f359247117d2544e0a98632014fb
Joonas Häkkinen <joonas@joonashakkinen.fi>
2020-03-22 17:01:36 +0200

    Debug

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,5 +1,6 @@
 const connection = process.env.DATABASE_URL;
 connection.concat(process.env.NODE_ENV === 'production' ? '?ssl=open' : '');
+console.log(process.env.NODE_ENV);
 console.log('connection string:', connection);
 
 module.exports = {
