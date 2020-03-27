f9201cc61b1014d1562d463921f9766641745fc9
Joonas Häkkinen <joonas@joonashakkinen.fi>
2020-03-27 21:32:27 +0200

    Add routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import HomeView from './components/views/HomeView';
 
 export default () => (
-  <>
+  <BrowserRouter>
     <CssBaseline />
-    Vyardage.
-  </>
+
+    <Switch>
+      <Route path="/" component={HomeView} />
+    </Switch>
+  </BrowserRouter>
 );
