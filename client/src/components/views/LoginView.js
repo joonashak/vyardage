10f187d033fe933fc4320e0cc62e99834410dd22
Joonas Häkkinen <joonas@joonashakkinen.fi>
2020-03-28 22:27:21 +0200

    Add login form

--- client/src/components/views/LoginView.js
+++ client/src/components/views/LoginView.js
@@ -1,10 +1,46 @@
 import React from 'react';
+import { useForm, Controller } from 'react-hook-form';
+import { TextField, Button } from '@material-ui/core';
 import Header from '../Header';
 
 
-export default () => (
-  <>
-    <Header />
-    Login view.
-  </>
-);
+export default () => {
+  const { handleSubmit, control, errors } = useForm({ mode: 'onBlur' });
+
+  return (
+    <>
+      <Header />
+      Login view.
+      <form>
+        <Controller
+          as={TextField}
+          name="username"
+          control={control}
+          defaultValue=""
+          label="Username"
+          autoComplete="username"
+          variant="filled"
+          rules={{ required: 'Username cannot be empty' }}
+          error={!!errors.username}
+          helperText={errors.username && errors.username.message}
+        />
+        <Controller
+          as={TextField}
+          name="password"
+          control={control}
+          defaultValue=""
+          label="Password"
+          autoComplete="current-password"
+          variant="filled"
+          type="password"
+          rules={{ required: 'Password cannot be empty' }}
+          error={!!errors.password}
+          helperText={errors.password && errors.password.message}
+        />
+        <Button onClick={handleSubmit((data) => console.log(data))}>
+          Log In
+        </Button>
+      </form>
+    </>
+  );
+};
