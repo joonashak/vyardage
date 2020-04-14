bfc104788f202b8a46bd6d5921829521e1b1918d
Joonas Häkkinen <joonas@joonashakkinen.fi>
2020-04-14 11:23:34 +0300

    Add linting to test package

--- e2e/cypress/integration/authentication.spec.js
+++ e2e/cypress/integration/authentication.spec.js
@@ -1,15 +1,15 @@
-/// <reference types="Cypress" />
+// / <reference types="Cypress" />
 
 const login = (username, password) => {
-  cy.visit('/login')
-  cy.get('input[name=username]').type(username)
-  cy.get('input[name=password]').type(password)
-  cy.contains('Log In').click()
-}
+  cy.visit('/login');
+  cy.get('input[name=username]').type(username);
+  cy.get('input[name=password]').type(password);
+  cy.contains('Log In').click();
+};
 
 describe('Authentication', () => {
-  const username = 'admin'
-  const password = '1234'
+  const username = 'admin';
+  const password = '1234';
 
   before(() => {
     // Don't fail on non-200 status codes to enable repetitive test running in development.
@@ -19,40 +19,40 @@ describe('Authentication', () => {
       url: '/api/v1/initialUser',
       body: { username, password },
       failOnStatusCode: false,
-    })
-  })
+    });
+  });
 
   it('Login page loads', () => {
-    cy.visit('/')
-    cy.contains('Login').click()
-    cy.url().should('include', '/login')
-  })
+    cy.visit('/');
+    cy.contains('Login').click();
+    cy.url().should('include', '/login');
+  });
 
   it('User can log in', () => {
-    login(username, password)
+    login(username, password);
 
-    cy.url().should('equal', Cypress.config().baseUrl + '/')
-    cy.getCookie('connect.sid').should('exist')
-    cy.get('body').should('not.contain', 'Login')
-  })
+    cy.url().should('equal', `${Cypress.config().baseUrl}/`);
+    cy.getCookie('connect.sid').should('exist');
+    cy.get('body').should('not.contain', 'Login');
+  });
 
   it('User can log out', () => {
-    login(username, password)
+    login(username, password);
 
-    cy.get('button[aria-label="Open Menu"]').click()
-    cy.contains('Log Out').click()
+    cy.get('button[aria-label="Open Menu"]').click();
+    cy.contains('Log Out').click();
 
-    cy.url().should('equal', Cypress.config().baseUrl + '/')
-    cy.contains('Login')
-  })
+    cy.url().should('equal', `${Cypress.config().baseUrl}/`);
+    cy.contains('Login');
+  });
 
   it('Incorrect username is rejected', () => {
-    login('asd', password)
-    cy.contains('Login failed')
-  })
+    login('asd', password);
+    cy.contains('Login failed');
+  });
 
   it('Incorrect password is rejected', () => {
-    login(username, 'asd')
-    cy.contains('Login failed')
-  })
-})
+    login(username, 'asd');
+    cy.contains('Login failed');
+  });
+});
