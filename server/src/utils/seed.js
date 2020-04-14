5cc19140216a71adf349f16e6e805b722b0b634a
Joonas Häkkinen <joonas@joonashakkinen.fi>
2020-04-14 19:22:17 +0300

    Add route to reset database

--- server/src/utils/seed.js
+++ server/src/utils/seed.js
@@ -1,5 +1,9 @@
 import Knex from 'knex';
 import knexConfig from '../../knexfile';
+import Shot from '../models/Shot';
+import Club from '../models/Club';
+import Ball from '../models/Ball';
+import User from '../models/User';
 
 export default (router) => {
   if (process.env.NODE_ENV === 'production') {
@@ -12,5 +16,17 @@ export default (router) => {
   router.get('/seed', async (req, res) => {
     const knex = Knex(knexConfig);
     res.send(await knex.seed.run());
+  });
+
+  /**
+   * Empty all rows from the database. Schema is preserved.
+   */
+  router.get('/reset', async (req, res) => {
+    await Shot.query().delete();
+    await Club.query().delete();
+    await Ball.query().delete();
+    await User.query().delete();
+
+    res.send('Database reset.');
   });
 };
